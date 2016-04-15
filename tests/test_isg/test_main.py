bd220d0c3ee37ff7257df95db5f72882e6ff5b3e
Kevin James <KevinJames@thekev.in>
2016-04-14 19:02:01 -0700

    add failing test for no duplicate items

---
+++ tests/test_isg/test_main.py
@@ -0,0 +1,13 @@
+import league_utils.isg as isg
+import league_utils.models as models
+import league_utils.sources as sources
+
+
+def test_no_duplicate_items():
+    champ = models.Champion(133, name='Quinn')
+    sources.champion_gg(champ)
+
+    for role in champ.roles:
+        set_ = isg.item_set(champ, role)
+        items = [item for block in set_[3] for item in block[1]]
+        assert len(items) == len(set(items))
