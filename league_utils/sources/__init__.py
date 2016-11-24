2f88d1e4227e6dd5f587272539ecae76ae38040d
Kevin James <KevinJames@thekev.in>
2016-11-24 13:55:04 -0800

    update champion.gg consumer

--- league_utils/sources/__init__.py
+++ league_utils/sources/__init__.py
@@ -6,9 +6,6 @@ import league_utils.models as models
 
 
 def champion_gg(champ):
-    def parse_id(src):
-        return int(src.split('/')[-1].split('.')[0])
-
     BUILD = '.col-md-7 .build-wrapper a img'
     ROLES = '.champion-profile ul li a h3'
     STARTS = '.col-md-5 .build-wrapper a img'
@@ -27,8 +24,8 @@ def champion_gg(champ):
         soup = bs4.BeautifulSoup(resp.text, 'html.parser')
 
         champ.builds[role] += [
-            models.Item(parse_id(x['src']), x['tooltip'])
+            models.Item(x['data-id'])
             for x in soup.select(BUILD)]
         champ.starts[role] += [
-            models.Item(parse_id(x['src']), x['tooltip'])
+            models.Item(x['data-id'])
             for x in soup.select(STARTS)]
