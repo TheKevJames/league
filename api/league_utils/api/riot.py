cc408c886b95a739f279cbbf0180a94fa25fdf61
Kevin James <KevinJames@thekev.in>
2017-01-13 18:42:27 -0500

    add get_all_champs in api.riot

--- api/league_utils/api/riot.py
+++ api/league_utils/api/riot.py
@@ -10,14 +10,24 @@ TOKEN = os.environ['RIOT_TOKEN']
 
 API_STATIC_DATA = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2'
 API_CHAMP = API_STATIC_DATA + '/champion/{}?champData=all&api_key=' + TOKEN
-API_ITEMS = API_STATIC_DATA + '/item?api_key=' + TOKEN
+API_CHAMPS = API_STATIC_DATA + '/champion/?api_key=' + TOKEN
 API_ITEM = API_STATIC_DATA + '/item/{}?itemData=all&api_key=' + TOKEN
+API_ITEMS = API_STATIC_DATA + '/item?api_key=' + TOKEN
 
 
 @async_lru_cache(maxsize=128)
 async def get_champ(cid):
     assert TOKEN
     url = API_CHAMP.format(cid)
+    async with aiohttp.ClientSession() as client, client.get(url) as response:
+        assert response.status == 200
+        return await response.json()
+
+
+@async_lru_cache(maxsize=2)
+async def get_champs():
+    assert TOKEN
+    url = API_CHAMPS
     async with aiohttp.ClientSession() as client, client.get(url) as response:
         assert response.status == 200
         return await response.json()
@@ -44,6 +54,7 @@ async def get_items():
 async def reset_cache():
     while True:
         get_champ.cache_clear()
+        get_champs.cache_clear()
         get_item.cache_clear()
         get_items.cache_clear()
 
