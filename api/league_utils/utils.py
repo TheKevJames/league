e747f2be7d1e994868515be1128eca7b169022e6
Kevin James <KevinJames@thekev.in>
2017-01-17 00:32:26 -0500

    add cache_info function to async_lru_cache, lock instances with same args

--- api/league_utils/utils.py
+++ api/league_utils/utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import collections
 import functools
 import operator
@@ -5,27 +6,34 @@ import operator
 
 def async_lru_cache(maxsize=128):
     cache = collections.OrderedDict()
+    locks = collections.defaultdict(asyncio.Lock)
 
     def cache_clear():
         nonlocal cache
         cache = collections.OrderedDict()
+
+    def cache_info():
+        Info = collections.namedtuple('cache_info', ['maxsize', 'cursize'])
+        return Info(maxsize, len(cache))
 
     def decorator(fn):
         @functools.wraps(fn)
         async def memoizer(*args, **kwargs):
             key = str((args, kwargs))
 
-            try:
-                cache[key] = cache.pop(key)
-            except KeyError:
-                if len(cache) >= maxsize:
-                    cache.popitem(last=False)
+            with await locks[key]:
+                try:
+                    cache[key] = cache.pop(key)
+                except KeyError:
+                    if len(cache) >= maxsize:
+                        cache.popitem(last=False)
 
-                cache[key] = await fn(*args, **kwargs)
+                    cache[key] = await fn(*args, **kwargs)
 
-            return cache[key]
+                return cache[key]
 
         memoizer.cache_clear = cache_clear
+        memoizer.cache_info = cache_info
         return memoizer
 
     return decorator
