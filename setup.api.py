a21c62e77b09126f954a9413a26e52cdfc5a8372
Kevin James <kevin@talkiq.com>
2017-08-06 23:26:19 -0700

    fix(ci): fix auto-deployment typos

--- setup.api.py
+++ setup.api.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='league-utils',
-    version='1.0.4',
+    version='1.0.5',
     packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
